52a0503e745ed331649cf70f46a83ab715a40014
FurbaSherpa <37233795+FurbaSherpa@users.noreply.github.com>
2018-12-07 16:28:19 -0500

    join page fixed

--- src/app/user/components/join/join.component.ts
+++ src/app/user/components/join/join.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup , FormBuilder, Validators} from '@angular/forms';
+import { FormGroup , FormBuilder, Validators, FormControl} from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PlayersService} from '../../../players.service';
@@ -13,9 +13,10 @@ export class JoinComponent implements OnInit {
 
   id: String;
   player: any = {};
+  joinForm: FormGroup;
 
   players: Player[];
-  displayedColumns = ['username', 'rank', 'score', 'time', 'favorite', 'status'];
+  displayedColumns = ['username', 'rank', 'score', 'time', 'favorite', 'actions'];
 
   constructor(private playersService: PlayersService, private router: Router, private route: ActivatedRoute, private snackBar: MatSnackBar, private fb : FormBuilder) {
     this.id = this.route.snapshot.paramMap.get('id'); 
@@ -23,36 +24,31 @@ export class JoinComponent implements OnInit {
 
    ngOnInit() {
     
-    // this.playersService.getPlayersByID(this.id).subscribe(res => {
-    //     this.player = res;
-    //     this.joinForm.get('username').setValue(this.player.username);
-    //     this.joinForm.get('rank').setValue(this.player.rank);
-    //     this.joinForm.get('score').setValue(this.player.score);
-    //     this.joinForm.get('time').setValue(this.player.time);
-    //     this.joinForm.get('favorite').setValue(this.player.favorite);
-    //     this.joinForm.get('customer').setValue(this.player.customer);
-    //     this.joinForm.get('status').setValue(this.player.status);
-    //   });
-    this.getOnePlayer();
-  }
-  fetchPlayers(){
-    this.playersService
-      .getPlayers()
-      .subscribe((data: Player[]) => {
-        this.players = data;
-        console.log('Data requested....');
-        console.log(this.players);
-      });
+     this.playersService.getPlayersByID(this.id).subscribe(res => {
+         this.player = res;
+         this.joinForm.get('username').setValue(this.player.username);
+         this.joinForm.get('rank').setValue(this.player.rank);
+         this.joinForm.get('score').setValue(this.player.score);
+         this.joinForm.get('time').setValue(this.player.time);
+         this.joinForm.get('favorite').setValue(this.player.favorite);
+         this.joinForm.get('customer').setValue(this.player.customer);
+         this.joinForm.get('status').setValue(this.player.status);
+       });
+       this.addForm();
+       this.joinForm.disable()
+
   }
 
-  getOnePlayer(){
-    this.playersService
-    .getPlayersByID(this.id)
-    .subscribe((data:Player[]) => {
-      this.players = data;
-      console.log(this.players);
+  addForm(){
+    this.joinForm = this.fb.group({
+      username: [this.player.username, Validators.required],
+      rank: '',
+      score:'',
+      time: '',
+      favorite: '',
+      customer: '',
+      status: ''
     });
-
-  }
+   }
 
 }
